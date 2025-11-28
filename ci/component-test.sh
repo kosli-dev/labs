cdb41c1dd0a3a287fa4fa12fe85fdf5036d16cf4
Dan Grøndahl <dan@kosli.com>
2025-11-28 10:54:24 +0100

    fix failing component test

--- ci/component-test.sh
+++ ci/component-test.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
 docker_username=$DockerRepo docker compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
